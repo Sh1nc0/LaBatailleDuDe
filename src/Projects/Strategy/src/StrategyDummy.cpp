47c2f85096cc02c012a9958e782b49bcf33dd88c
Theo LE BAIL <theo.le-bail@etu.univ-nantes.fr>
2023-11-22 09:36:58 +0100

    dummy strategie

--- src/Projects/Strategy/src/StrategyDummy.cpp
+++ src/Projects/Strategy/src/StrategyDummy.cpp
@@ -1,21 +1,84 @@
 #include "StrategyDummy.h"
+#include <iostream>
+#include <vector>
 
-StrategyDummy::StrategyDummy(unsigned int id, unsigned int nbPlayer, const SMap* /*map*/) :
+StrategyDummy::StrategyDummy(unsigned int id, unsigned int nbPlayer, const SMap* map) :
 	Id(id),
 	NbPlayer(nbPlayer)
 {
-	// faire une copie enti�re de la structure map localement dans l'objet Map
-	Map.cells = nullptr;
-	Map.nbCells = 0;
+	Map.cells = new SCell[map->nbCells];
+	Map.nbCells = map->nbCells;
+	for (size_t i = 0; i < Map.nbCells; i++)
+	{
+		Map.cells[i].infos = map->cells[i].infos;
+		Map.cells[i].nbNeighbors = map->cells[i].nbNeighbors;
+		Map.cells[i].neighbors = new pSCell[map->cells[i].nbNeighbors];
+		for (size_t j = 0; j < Map.cells[i].nbNeighbors; j++)
+		{
+			Map.cells[i].neighbors[j] = &Map.cells[map->cells[i].neighbors[j]->infos.id];
+		}
+
+	}
 }
 
 StrategyDummy::~StrategyDummy()
 {
-	// détruire proprement la structure Map
+	for (size_t i = 0; i < Map.nbCells; i++)
+	{
+		delete[] Map.cells[i].neighbors;
+	}
+	delete[] Map.cells;
 }
 
-bool StrategyDummy::PlayTurn(unsigned int /*gameTurn*/, const SGameState* /*state*/, STurn* /*turn*/)
+bool StrategyDummy::PlayTurn(unsigned int gameTurn, const SGameState* state, STurn* turn)
 {
-	// dérouler votre algorithme de choix
-	return(false);
+	for (int i = 0; i < Map.nbCells; ++i) {
+		Map.cells[i].infos = state->cells[i];
+	}
+	std::vector<pSCell> regionPlayable;
+	for (int i = 0; i < Map.nbCells; ++i) {
+		if (Map.cells[i].infos.owner == Id && Map.cells[i].infos.nbDices > 1) {
+			for (unsigned j = 0; j < Map.cells[i].nbNeighbors; ++j) {
+				if (Map.cells[i].neighbors[j]->infos.owner != Id) {
+					regionPlayable.push_back(&Map.cells[i]);
+					break;
+				}
+			}
+		}
+	}
+
+	if (regionPlayable.empty()) {
+		std::cout << "fin du tour pas de coup possible" << std::endl;
+		return false;
+	}
+
+	auto startingRegionId = regionPlayable[0]->infos.id;
+	for (auto& it : regionPlayable) {
+		if (it->infos.nbDices > Map.cells[startingRegionId].infos.nbDices) {
+			startingRegionId = it->infos.id;
+		}
+	}
+
+	std::cout << "nombre de" << Map.cells[startingRegionId].infos.nbDices << std::endl;
+	auto& startingRegion = Map.cells[startingRegionId];
+	std::vector<pSCell> cellsAttackable;
+	for (unsigned i = 0; i < startingRegion.nbNeighbors; ++i) {
+		if (startingRegion.neighbors[i]->infos.owner != Id) {
+			cellsAttackable.push_back(startingRegion.neighbors[i]);
+			break;
+		}
+	}
+	if (cellsAttackable.empty()) {
+		return false;
+	}
+	auto endingRegion = cellsAttackable[0]->infos.id;
+	for (auto& it : cellsAttackable) {
+		if (it->infos.nbDices > Map.cells[endingRegion].infos.nbDices) {
+			endingRegion = it->infos.id;
+		}
+	}
+	turn->cellFrom = startingRegionId;
+	turn->cellTo = endingRegion;
+	std::cout << "fin du tour" << std::endl;
+	return true;
 }
